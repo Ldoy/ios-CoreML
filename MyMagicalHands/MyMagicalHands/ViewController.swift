4afd4e11ca069b89d29adaf0e6b11ea1f97506bd
Ldoy <leedoy7989@gmail.com>
2022-02-05 14:56:05 +0900

    Feat: Add MLmodel to XCode Project

---
+++ MyMagicalHands/MyMagicalHands/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MyMagicalHands
+//
+//  Created by Do Yi Lee on 2022/02/05.
+//
+
+import UIKit
+import CoreML
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        
+    }
+
+
+}
+
