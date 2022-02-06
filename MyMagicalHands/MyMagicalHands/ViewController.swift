eaf6b481ff811088504935ce7656ae4becfb755f
Ldoy <leedoy7989@gmail.com>
2022-02-06 15:43:45 +0900

    Feat: Implement background pattern

--- MyMagicalHands/MyMagicalHands/ViewController.swift
+++ MyMagicalHands/MyMagicalHands/ViewController.swift
@@ -24,10 +24,56 @@ class ViewController: UIViewController {
 
 extension ViewController {
     private func renderBackgroundImage() {
-        guard let backgroundImage = background else  {
+        let numberOfImageAsPattern = CGFloat(100.0)
+        guard let backgroundImage = background?.resizeImage(numberOfImageAsPattern,
+                                                            opaque: false) else  {
             return
         }
         
         self.view.backgroundColor = UIColor(patternImage: backgroundImage)
     }
 }
+
+extension UIImage {
+      func resizeImage(_ dimension: CGFloat,
+                       opaque: Bool,
+                       contentMode: UIView.ContentMode = .scaleAspectFill) -> UIImage {
+          var width: CGFloat
+          var height: CGFloat
+          var newImage: UIImage
+
+          let size = self.size
+          let aspectRatio =  size.width/size.height
+
+          switch contentMode {
+              case .scaleAspectFill:
+                  if aspectRatio > 1 {                            // Landscape image
+                      width = dimension
+                      height = dimension / aspectRatio
+                  } else {                                        // Portrait image
+                      height = dimension
+                      width = dimension * aspectRatio
+                  }
+
+          default:
+              fatalError("UIIMage.resizeToFit(): FATAL: Unimplemented ContentMode")
+          }
+
+          if #available(iOS 10.0, *) {
+              let renderFormat = UIGraphicsImageRendererFormat.default()
+              renderFormat.opaque = opaque
+              let renderer = UIGraphicsImageRenderer(size: CGSize(width: width, height: height), format: renderFormat)
+              newImage = renderer.image {
+                  (context) in
+                  self.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+              }
+          } else {
+              UIGraphicsBeginImageContextWithOptions(CGSize(width: width, height: height), opaque, 0)
+                  self.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+                  newImage = UIGraphicsGetImageFromCurrentImageContext()!
+              UIGraphicsEndImageContext()
+          }
+
+          return newImage
+      }
+  }
